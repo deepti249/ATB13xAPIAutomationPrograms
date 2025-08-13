11ac2029bb7623245aa521800e95acc052d84026
deepti249 <deepti24.lodha@gmail.com>
2025-08-13 08:17:45 +0530

    commit

---
+++ src/test/java/com/deepti/sample/ex_07_Payload_management/Class/manualWay/requestPOJO/BookingDates.java
@@ -0,0 +1,22 @@
+package com.deepti.sample.ex_07_Payload_management.Class.manualWay.requestPOJO;
+
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+}
---
+++ src/test/java/com/deepti/sample/ex_07_Payload_management/Class/tools/responsePOJO/BookingResponse.java
@@ -0,0 +1,36 @@
+package com.deepti.sample.ex_07_Payload_management.Class.tools.responsePOJO;
+
+import com.deepti.sample.ex_07_Payload_management.Class.tools.requestPOJO.Booking;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class BookingResponse {
+    private Integer bookingid;
+    private Booking booking;
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    public Integer getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(Integer bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
